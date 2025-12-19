4c4fa99a2d595a27aef45cc32a76f39f2f7c2b84
Vinnie Magro <vmagro@meta.com>
2025-12-19 08:25:34 -0800

    [antlir2][image_test] fix /dev/null test
    Summary: This didn't start with `test_` so was never actually run...
    
    Test Plan:
    ```
    ❯ buck2 test fbcode//antlir/antlir2/testing/tests: -- dev_null
    Buck UI: https://www.internalfb.com/buck2/213286ba-372c-4c11-84fb-091de9b73934
    Network: Up: 169KiB  Down: 278KiB  (reSessionID-9884d4b9-018c-42b0-9547-a7be2ad6bdfc)
    Analyzing targets. Remaining     0/34                                                                                                                                       34 actions
    Executing actions. Remaining     0/664                                                                                                                                      9:17.1s exec time total
    Command: test.     Finished 388 local
    Time elapsed: 40.2s
    Tests finished: Pass 24. Fail 0. Fatal 0. Skip 0. Omit 0. Infra Failure 0. Build failure 0
    ```
    https://www.internalfb.com/intern/testinfra/testrun/5066549913236818
    
    Reviewed By: justintrudell
    
    Differential Revision: D89397424
    
    fbshipit-source-id: 93a130137af738af0b5f646f0430e3bb204be28c

--- antlir/antlir2/testing/tests/test.py
+++ antlir/antlir2/testing/tests/test.py
@@ -25,11 +25,11 @@ class Test(unittest.TestCase):
     def test_json_env_quoting(self) -> None:
         self.assertEqual({"foo": "bar"}, json.loads(os.getenv("JSON_ENV")))
 
-    def dev_null(self) -> None:
+    def test_dev_null(self) -> None:
         st = os.stat("/dev/null")
         self.assertTrue(st.st_mode & stat.S_IFCHR, "/dev/null should be char device")
         self.assertEqual(
-            st.st_rdev & stat.S_IFCHR,
+            st.st_rdev,
             os.makedev(1, 3),
             "/dev/null device number is wrong",
         )
